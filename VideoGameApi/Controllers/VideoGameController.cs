7b159dbe9982a2202c433540769cd8670640b6d4
Sanju <gamingsanju15062001@gmail.com>
2025-04-15 23:59:23 +0530

    Add Static list of video Games for testing

--- VideoGameApi/Controllers/VideoGameController.cs
+++ VideoGameApi/Controllers/VideoGameController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using VideoGameApi.Models;
 
 namespace VideoGameApi.Controllers
 {
@@ -7,6 +8,17 @@ namespace VideoGameApi.Controllers
     [ApiController]
     public class VideoGameController : ControllerBase
     {
-
+        // creating static list of video games for testing purposes 
+        static private List<VideoGame> videoGames = new List<VideoGame>
+        {
+            new VideoGame { Id = 1, Title = "The Legend of Zelda: Breath of the Wild", Platform = "Nintendo Switch", Developer = "Nintendo", Publisher = "Nintendo", Genre = "Action-adventure" },
+            new VideoGame { Id = 2, Title = "The Witcher 3: Wild Hunt", Platform = "PC, PS4, Xbox One, Nintendo Switch", Developer = "CD Projekt Red", Publisher = "CD Projekt", Genre = "Action role-playing" },
+            new VideoGame { Id = 3, Title = "Minecraft", Platform = "PC, PS4, Xbox One, Nintendo Switch, Mobile", Developer = "Mojang Studios", Publisher = "Mojang Studios", Genre = "Sandbox" }
+        };
+        [HttpGet]
+        public ActionResult<List<VideoGame>> GetVideoGames()
+        {
+            return Ok(videoGames);
+        }
     }
 }
--- VideoGameApi/Program.cs
+++ VideoGameApi/Program.cs
@@ -1,16 +1,19 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi("v1");
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.MapScalarApiReference();
     app.MapOpenApi();
 }
 
