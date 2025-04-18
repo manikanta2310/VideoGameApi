6124cd4f19e3f1ecd9c2f3fb8e79787761a91fd3
Sanju <gamingsanju15062001@gmail.com>
2025-04-18 17:06:47 +0530

    adding dbcontext

---
+++ VideoGameApi/Data/VideoGameDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGameApi.Models;
+
+namespace VideoGameApi.Data
+{
+    public class VideoGameDbContext(DbContextOptions<VideoGameDbContext> options) : DbContext(options)
+    {
+        public DbSet<VideoGame> VideoGames { get; set; }
+
+
+    }
+}
